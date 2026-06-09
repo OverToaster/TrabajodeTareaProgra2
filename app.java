8a5661549c54fe8d4ff37ed973a1d72573caee15
AdrianoDS <adrianosb2008@gmail.com>
2026-06-09 12:49:39 -0600

    Creation of the class and addition of the code
    The main class has been created with the name app.Java where the code line has been added to ask the user 5 simple questions: what is your name, last name, email, phone number, and address.

---
+++ app.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner dato = new Scanner(System.in);
+        
+        System.out.println("Cual es tu nombre: ");
+        String nombre = dato.nextLine();
+        
+        System.out.println("Cual es tu apellido: ");
+        String apellido = dato.nextLine();
+        
+        System.out.println("Cual es tu correo: ");
+        String correo = dato.nextLine();
+        
+        System.out.println("Cual es tu numero de telefono: ");
+        String telefono = dato.nextLine();
+        
+        System.out.println("Cual es tu direccion: ");
+        String direccion = dato.nextLine();
+        
+        
+        
+        
+    }   
+}
